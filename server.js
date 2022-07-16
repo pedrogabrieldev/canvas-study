631577c79541553d3c779e6bf8658fbfbab3ea70
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-07-16 19:31:43 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.use('/', express.static('src'))
+app.use('/circular-movement', express.static('src/pages/circular-movement'))
+app.use('/gravity', express.static('src/pages/gravity'))
+
+app.listen(port, () => console.log(`Server listening on port ${port}`))
