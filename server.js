af805715b972588436444b0064a3d7b6e3d1e3ad
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-07-16 19:47:02 -0300

    Set the correct server port variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use('/', express.static('src'))
 app.use('/circular-movement', express.static('src/pages/circular-movement'))
