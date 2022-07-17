a29ddfd15161bb68f2ceb4416bb387602da46f58
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-07-17 10:18:22 -0300

    Implement gravity example

--- src/pages/gravity/script.js
+++ src/pages/gravity/script.js
@@ -8,3 +8,62 @@ window.addEventListener('resize', () => {
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
 })
+
+const gravity = 1
+const friction = 0.9
+
+let objects = []
+
+function Object(x, y, dx, dy, radius, color) {
+  this.x = x
+  this.y = y
+  this.dx = dx
+  this.dy = dy
+  this.radius = radius
+  this.color = color
+
+  this.update = () => {
+    if (this.y + this.radius + this.dy > canvas.height) {
+      this.dy *= -friction
+    } else {
+      this.dy += gravity
+    }
+
+    this.y += this.dy
+
+    this.draw()
+  }
+
+  this.draw = () => {
+    context.beginPath()
+    context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
+    context.fillStyle = this.color
+    context.fill()
+    context.closePath()
+  }
+}
+
+function init() {
+  let object1 = new Object(
+    canvas.width / 2,
+    canvas.height / 2,
+    0,
+    2,
+    10,
+    'blue'
+  )
+
+  objects.push(object1)
+
+  animate()
+}
+
+function animate() {
+  requestAnimationFrame(animate)
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  context.fillStyle = 'black'
+  context.fillRect(0, 0, canvas.width, canvas.height)
+  objects.map((object) => object.update())
+}
+
+init()
