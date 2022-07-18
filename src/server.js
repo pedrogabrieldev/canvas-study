1c243e827372ff2da879f88bf9b00f16f048f2e1
Pedro Gabriel <pedrogabrieldev@gmail.com>
2022-07-18 01:27:21 -0300

    Simplify folder structure

--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-app.use('/', express.static('src/pages/main'))
-app.use('/circular-movement', express.static('src/pages/circular-movement'))
-app.use('/gravity', express.static('src/pages/gravity'))
+app.use('/', express.static('src/pages'))
 
 app.listen(port, () => console.log(`Server listening on port ${port}`))
